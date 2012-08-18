a8a7cb53f00555612005bf1bed52398b6affb1dc
Dustin Getz <dustin.getz@gmail.com>
2012-08-18 15:05:08 -0400

    better names for util fns in environment

--- environment.py
+++ environment.py
@@ -5,7 +5,7 @@ class _Environment_m(Monad):
     "writer inside reader, for maps"
 
     def unit(self, v):
-        return lambda env: (v, {})
+        return lambda env: (v, env)
 
     def bind(self, mv, mf):
         def _(env):
@@ -15,16 +15,16 @@ class _Environment_m(Monad):
 
 env_m = _Environment_m()
 
-def ask(env): return env, env
+def env_ask(env): return env, env
 
-def set(pair):
+def env_set(pair):
     def _(env):
         newenv = env.copy()
         newenv.update([pair])
         return None, newenv
     return _
 
-def get(sym):
+def env_get(sym):
     def _(env):
         return env[sym], env
     return _
@@ -37,17 +37,17 @@ def test():
     unit = env_m.unit
     fmap = env_m.fmap
 
-    r = bind(  set(("a", 2)),    lambda _:
-        bind(  set(("b", 3)),    lambda _:
-        bind(  get("a"),         lambda a:
-        bind(  get("b"),         lambda b:
-        bind(  set(("c", a+b)),  lambda _:
-        bind(  get("c"),         lambda c:
-               unit(c)           ))))))
+    r = bind(  env_set(("a", 2)),    lambda _:
+        bind(  env_set(("b", 3)),    lambda _:
+        bind(  env_get("a"),         lambda a:
+        bind(  env_get("b"),         lambda b:
+        bind(  env_set(("c", a+b)),  lambda _:
+        bind(  env_get("c"),         lambda c:
+               unit(c)               ))))))
 
     assert r({}) == unit(5)({})
 
-    r = bind(get("c"), lambda c: unit(c))
+    r = bind(env_get("c"), lambda c: unit(c))
     assert r({"c":42}) == unit(42)({})
     assert r({"c":43}) == unit(43)({})
 
